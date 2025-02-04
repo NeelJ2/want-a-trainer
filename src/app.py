d379f9a4af6e3469a9fa6b8a94963badf069acd5
njoshi24 <njoshi24@terpmail.umd.edu>
2025-02-03 21:53:23 -0500

    stylistic stuff done

--- src/app.py
+++ src/app.py
@@ -3,5 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submittedform')
+def home():
+    return render_template('')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
